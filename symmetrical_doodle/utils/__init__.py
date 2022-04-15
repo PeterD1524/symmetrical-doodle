7879caf84ffaedd18791487ea1662e106185d936
PeterD1524 <qaz246135@gmail.com>
2022-04-15 08:07:09 +0800

    send swipe

--- symmetrical_doodle/utils/__init__.py
+++ symmetrical_doodle/utils/__init__.py
@@ -1,3 +1,6 @@
+import asyncio
+import time
+
 import symmetrical_doodle.android.input
 import symmetrical_doodle.control_message
 import symmetrical_doodle.controllers
@@ -10,11 +13,19 @@ async def turn_screen_off(
     message = symmetrical_doodle.control_message.SetScreenPowerMode(
         symmetrical_doodle.control_message.ScreenPowerMode.OFF
     )
-    await controller.push_message(message)
+    await controller.send_message(message)
+
+
+def lerp(a: float, b: float, alpha: float):
+    return (b - a) * alpha + a
+
+
+def run_tap():
+    pass
 
 
 # https://android.googlesource.com/platform/frameworks/base/+/android-7.1.1_r22/cmds/input/src/com/android/commands/input/Input.java
-def send_tap(
+async def send_tap(
     controller: symmetrical_doodle.controllers.Controller,
     position: symmetrical_doodle.coords.Position
 ):
@@ -27,7 +38,7 @@ def send_tap(
         buttons=symmetrical_doodle.android.input.MotionEventButton.
         AMOTION_EVENT_BUTTON_PRIMARY.value,
     )
-    controller.push_message_nowait(message)
+    await controller.send_message(message)
     message = symmetrical_doodle.control_message.InjectTouchEvent(
         action=symmetrical_doodle.android.input.MotionEventAction.
         AMOTION_EVENT_ACTION_UP,
@@ -37,4 +48,63 @@ def send_tap(
         buttons=symmetrical_doodle.android.input.MotionEventButton.
         AMOTION_EVENT_BUTTON_PRIMARY.value,
     )
-    controller.push_message_nowait(message)
+    await controller.send_message(message)
+
+
+async def send_swipe_bad(
+    controller: symmetrical_doodle.controllers.Controller,
+    screen_size: symmetrical_doodle.coords.Size,
+    x1: int,
+    y1: int,
+    x2: int,
+    y2: int,
+    duration: float = 0.3,
+    interval: float = 0.005,
+):
+    down = time.time()
+    message = symmetrical_doodle.control_message.InjectTouchEvent(
+        action=symmetrical_doodle.android.input.MotionEventAction.
+        AMOTION_EVENT_ACTION_DOWN,
+        pointer_id=symmetrical_doodle.control_message.POINTER_ID_MOUSE,
+        position=symmetrical_doodle.coords.Position(
+            screen_size, symmetrical_doodle.coords.Point(x1, y1)
+        ),
+        pressure=1.0,
+        buttons=symmetrical_doodle.android.input.MotionEventButton.
+        AMOTION_EVENT_BUTTON_PRIMARY.value,
+    )
+    await controller.send_message(message)
+    end_time = down + duration
+    now = time.time()
+    while now < end_time:
+        elapsed_time = now - down
+        alpha = elapsed_time / duration
+        message = symmetrical_doodle.control_message.InjectTouchEvent(
+            action=symmetrical_doodle.android.input.MotionEventAction.
+            AMOTION_EVENT_ACTION_MOVE,
+            pointer_id=symmetrical_doodle.control_message.POINTER_ID_MOUSE,
+            position=symmetrical_doodle.coords.Position(
+                screen_size,
+                symmetrical_doodle.coords.Point(
+                    round(lerp(x1, x2, alpha)), round(lerp(y1, y2, alpha))
+                )
+            ),
+            pressure=1.0,
+            buttons=symmetrical_doodle.android.input.MotionEventButton.
+            AMOTION_EVENT_BUTTON_PRIMARY.value,
+        )
+        await controller.send_message(message)
+        await asyncio.sleep(interval)
+        now = time.time()
+    message = symmetrical_doodle.control_message.InjectTouchEvent(
+        action=symmetrical_doodle.android.input.MotionEventAction.
+        AMOTION_EVENT_ACTION_UP,
+        pointer_id=symmetrical_doodle.control_message.POINTER_ID_MOUSE,
+        position=symmetrical_doodle.coords.Position(
+            screen_size, symmetrical_doodle.coords.Point(x2, y2)
+        ),
+        pressure=1.0,
+        buttons=symmetrical_doodle.android.input.MotionEventButton.
+        AMOTION_EVENT_BUTTON_PRIMARY.value,
+    )
+    await controller.send_message(message)
