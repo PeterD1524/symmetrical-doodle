24eedef88080051985ed355bf8d55bcec85a98c3
PeterD1524 <qaz246135@gmail.com>
2022-04-29 01:19:53 +0800

    update test for parse_device_ip_from_output

--- tests/test_adb_utils.py
+++ tests/test_adb_utils.py
@@ -6,39 +6,19 @@ import symmetrical_doodle.adb.utils
 @pytest.mark.parametrize(
     'output,expected', [
         (
-            b'192.168.1.0/24 dev wlan0  proto kernel  scope link  src 192.168.12.34\r\r\n',
+            b'192.168.1.0/24  proto kernel  scope link  src 192.168.12.34\r\r\n',
             b'192.168.12.34'
         ),
         (
-            b'192.168.1.0/24 dev wlan0  proto kernel  scope link  src 192.168.12.34',
+            b'192.168.1.0/24  proto kernel  scope link  src 192.168.12.34',
             b'192.168.12.34'
         ),
         (
-            b'192.168.1.0/24 dev wlan0  proto kernel  scope link  src 192.168.12.34 \n',
+            b'192.168.1.0/24  proto kernel  scope link  src 192.168.12.34 \n',
             b'192.168.12.34'
-        ),
-        (
-            b'192.168.1.0/24 dev wlan0  proto kernel  scope link  src 192.168.1.2\r\n10.0.0.0/24 dev rmnet  proto kernel  scope link  src 10.0.0.2\r\n',
-            b'192.168.1.2'
-        ),
-        (
-            b'10.0.0.0/24 dev rmnet  proto kernel  scope link  src 10.0.0.3\r\n192.168.1.0/24 dev wlan0  proto kernel  scope link  src 192.168.1.3\r\n',
-            b'192.168.1.3'
         )
     ]
 )
 def test_parse_device_ip_from_output(output: bytes, expected: bytes):
     ip = symmetrical_doodle.adb.utils.parse_device_ip_from_output(output)
     assert ip == expected
-
-
-@pytest.mark.parametrize(
-    'output', [
-        b'192.168.1.0/24 dev rmnet  proto kernel  scope link  src 192.168.12.34\r\r\n',
-        b'192.168.1.0/24 dev rmnet  proto kernel  scope link  src 192.168.12.34',
-        b'192.168.1.0/24 dev rmnet  proto kernel  scope link  src \n'
-    ]
-)
-def test_parse_device_ip_from_output_value_error(output: bytes):
-    with pytest.raises(ValueError):
-        symmetrical_doodle.adb.utils.parse_device_ip_from_output(output)
