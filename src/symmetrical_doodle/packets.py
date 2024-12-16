4be75cf4ed21ec397e75efd757c40f59d96346ee
PeterD1524 <53310459+PeterD1524@users.noreply.github.com>
2024-12-17 03:53:41 +0800

    demuxer push codec context to packet sinks (#10)
    Signed-off-by: PeterD1524 <qaz246135@gmail.com>

--- src/symmetrical_doodle/packets.py
+++ src/symmetrical_doodle/packets.py
@@ -2,6 +2,8 @@ import dataclasses
 from typing import Any
 
 import av
+import av.codec
+import av.codec.context
 
 AV_PKT_FLAG_KEY = 0x0001
 
@@ -23,3 +25,20 @@ class Packet:
     @property
     def size(self):
         return len(self.input)
+
+
+@dataclasses.dataclass
+class VideoCodecContext:
+    codec: av.codec.Codec
+    flags: int
+    width: int
+    height: int
+    pix_fmt: str
+
+    def create_av_codec_context(self):
+        context = av.codec.context.CodecContext.create(self.codec)
+        context.flags |= self.flags
+        context.width = self.width
+        context.height = self.height
+        context.pix_fmt = self.pix_fmt
+        return context
