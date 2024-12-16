051041e024b222cc3db0e66ad2653669d69739a0
PeterD1524 <53310459+PeterD1524@users.noreply.github.com>
2024-12-17 01:47:33 +0800

    update some protocol stuff to scrcpy 3.1 (#7)
    device info now only contains device name
    demuxer will receive codex id then for video it will receive width and height
    
    Signed-off-by: PeterD1524 <qaz246135@gmail.com>

--- src/symmetrical_doodle/demuxers.py
+++ src/symmetrical_doodle/demuxers.py
@@ -1,5 +1,6 @@
 import asyncio
 import dataclasses
+import enum
 
 import symmetrical_doodle.packets
 import symmetrical_doodle.utils.buffer
@@ -10,6 +11,31 @@ PACKET_FLAG_CONFIG = 1 << 63
 PACKET_FLAG_KEY_FRAME = 1 << 62
 
 PACKET_PTS_MASK = PACKET_FLAG_KEY_FRAME - 1
+
+
+def to_av_codec_id(codec_id: int):
+    class CodecID(enum.Enum):
+        H264 = 0x68323634  # "h264" in ASCII
+        H265 = 0x68323635  # "h265" in ASCII
+        AV1 = 0x00617631  # "av1" in ASCII
+        OPUS = 0x6F707573  # "opus" in ASCII
+        AAC = 0x00616163  # "aac" in ASCII
+        FLAC = 0x666C6163  # "flac" in ASCII
+        RAW = 0x00726177  # "raw" in ASCII
+
+    av_codec_names = {
+        CodecID.H264.value: "h264",
+        CodecID.H265.value: "hevc",
+        CodecID.AV1.value: "av1",
+        CodecID.OPUS.value: "opus",
+        CodecID.AAC.value: "aac",
+        CodecID.FLAC.value: "flac",
+        CodecID.RAW.value: "pcm_s16le",
+    }
+    try:
+        return av_codec_names[codec_id]
+    except KeyError:
+        return None
 
 
 @dataclasses.dataclass
@@ -24,7 +50,29 @@ class Demuxer:
         default=None, init=False
     )
 
+    async def receive_codec_id(self):
+        reader, _ = self.connection
+        return symmetrical_doodle.utils.buffer.read32be(await reader.readexactly(4), 0)[
+            0
+        ]
+
+    async def receive_video_size(self):
+        reader, _ = self.connection
+        data = await reader.readexactly(8)
+        start = 0
+        width, start = symmetrical_doodle.utils.buffer.read32be(data, start)
+        height, start = symmetrical_doodle.utils.buffer.read32be(data, start)
+        return width, height
+
     async def run(self):
+        raw_codec_id = await self.receive_codec_id()
+        assert raw_codec_id not in (0, 1)
+        codec_id = to_av_codec_id(raw_codec_id)
+        assert codec_id is not None
+
+        if True:
+            width, height = await self.receive_video_size()
+
         while True:
             packet = await self.receive_packet()
             await self.push_packet(packet)
